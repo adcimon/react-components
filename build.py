9763ead506e2b068eb7b8030db416ed78ecc9f82
adcimon <37084114+adcimon@users.noreply.github.com>
2023-04-14 15:48:53 +0200

    Added styles to build

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ build_dir = 'dist'
 
 def main():
 	subprocess.call('tsc', shell=True)
+	shutil.copytree('src/styles', build_dir + "/styles")
 	shutil.copy('package.json', build_dir)
 	shutil.copy('README.md', build_dir)
 	shutil.copy('LICENSE.md', build_dir)
